243e8169496f9889a358121ae1ba6cc10a508e74
unknown <imzioeren@gmail.com>
2020-11-01 00:12:47 +0100

    Simple project made in Visual Basic

---
+++ EasyImageConverter.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class EasyImageConverter
+    Inherits MaterialSkin.Controls.MaterialForm
+
+    'Form esegue l'override del metodo Dispose per pulire l'elenco dei componenti.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Richiesto da Progettazione Windows Form
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: la procedura che segue è richiesta da Progettazione Windows Form
+    'Può essere modificata in Progettazione Windows Form.  
+    'Non modificarla mediante l'editor del codice.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(EasyImageConverter))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SaveFileDialog1 = New System.Windows.Forms.SaveFileDialog()
+        Me.MaterialButton1 = New MaterialSkin.Controls.MaterialButton()
+        Me.MaterialButton2 = New MaterialSkin.Controls.MaterialButton()
+        Me.MaterialButton3 = New MaterialSkin.Controls.MaterialButton()
+        Me.MaterialTextBox1 = New MaterialSkin.Controls.MaterialTextBox()
+        Me.MaterialTextBox2 = New MaterialSkin.Controls.MaterialTextBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Location = New System.Drawing.Point(12, 86)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 14)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Input image:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Location = New System.Drawing.Point(12, 134)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(98, 14)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Output image:"
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Title = "Open image..."
+        '
+        'SaveFileDialog1
+        '
+        Me.SaveFileDialog1.Title = "Save image..."
+        '
+        'MaterialButton1
+        '
+        Me.MaterialButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialButton1.Depth = 0
+        Me.MaterialButton1.DrawShadows = True
+        Me.MaterialButton1.HighEmphasis = True
+        Me.MaterialButton1.Icon = Nothing
+        Me.MaterialButton1.Location = New System.Drawing.Point(191, 168)
+        Me.MaterialButton1.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.MaterialButton1.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialButton1.Name = "MaterialButton1"
+        Me.MaterialButton1.Size = New System.Drawing.Size(173, 36)
+        Me.MaterialButton1.TabIndex = 6
+        Me.MaterialButton1.Text = "convert this image"
+        Me.MaterialButton1.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained
+        Me.MaterialButton1.UseAccentColor = False
+        Me.MaterialButton1.UseVisualStyleBackColor = True
+        '
+        'MaterialButton2
+        '
+        Me.MaterialButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialButton2.Depth = 0
+        Me.MaterialButton2.DrawShadows = True
+        Me.MaterialButton2.HighEmphasis = True
+        Me.MaterialButton2.Icon = Nothing
+        Me.MaterialButton2.Location = New System.Drawing.Point(482, 75)
+        Me.MaterialButton2.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.MaterialButton2.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialButton2.Name = "MaterialButton2"
+        Me.MaterialButton2.Size = New System.Drawing.Size(33, 36)
+        Me.MaterialButton2.TabIndex = 7
+        Me.MaterialButton2.Text = "..."
+        Me.MaterialButton2.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained
+        Me.MaterialButton2.UseAccentColor = False
+        Me.MaterialButton2.UseVisualStyleBackColor = True
+        '
+        'MaterialButton3
+        '
+        Me.MaterialButton3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.MaterialButton3.Depth = 0
+        Me.MaterialButton3.DrawShadows = True
+        Me.MaterialButton3.HighEmphasis = True
+        Me.MaterialButton3.Icon = Nothing
+        Me.MaterialButton3.Location = New System.Drawing.Point(482, 123)
+        Me.MaterialButton3.Margin = New System.Windows.Forms.Padding(4, 6, 4, 6)
+        Me.MaterialButton3.MouseState = MaterialSkin.MouseState.HOVER
+        Me.MaterialButton3.Name = "MaterialButton3"
+        Me.MaterialButton3.Size = New System.Drawing.Size(33, 36)
+        Me.MaterialButton3.TabIndex = 8
+        Me.MaterialButton3.Text = "..."
+        Me.MaterialButton3.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained
+        Me.MaterialButton3.UseAccentColor = False
+        Me.MaterialButton3.UseVisualStyleBackColor = True
+        '
+        'MaterialTextBox1
+        '
+        Me.MaterialTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.MaterialTextBox1.Depth = 0
+        Me.MaterialTextBox1.Font = New System.Drawing.Font("Roboto", 12.0!)
+        Me.MaterialTextBox1.Location = New System.Drawing.Point(119, 75)
+        Me.MaterialTextBox1.MaxLength = 260
+        Me.MaterialTextBox1.MouseState = MaterialSkin.MouseState.OUT
+        Me.MaterialTextBox1.Multiline = False
+        Me.MaterialTextBox1.Name = "MaterialTextBox1"
+        Me.MaterialTextBox1.Size = New System.Drawing.Size(356, 36)
+        Me.MaterialTextBox1.TabIndex = 9
+        Me.MaterialTextBox1.Text = ""
+        Me.MaterialTextBox1.UseTallSize = False
+        '
+        'MaterialTextBox2
+        '
+        Me.MaterialTextBox2.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.MaterialTextBox2.Depth = 0
+        Me.MaterialTextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!)
+        Me.MaterialTextBox2.Location = New System.Drawing.Point(119, 123)
+        Me.MaterialTextBox2.MaxLength = 260
+        Me.MaterialTextBox2.MouseState = MaterialSkin.MouseState.OUT
+        Me.MaterialTextBox2.Multiline = False
+        Me.MaterialTextBox2.Name = "MaterialTextBox2"
+        Me.MaterialTextBox2.Size = New System.Drawing.Size(356, 36)
+        Me.MaterialTextBox2.TabIndex = 10
+        Me.MaterialTextBox2.Text = ""
+        Me.MaterialTextBox2.UseTallSize = False
+        '
+        'EasyImageConverter
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 14.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(534, 218)
+        Me.Controls.Add(Me.MaterialTextBox2)
+        Me.Controls.Add(Me.MaterialTextBox1)
+        Me.Controls.Add(Me.MaterialButton3)
+        Me.Controls.Add(Me.MaterialButton2)
+        Me.Controls.Add(Me.MaterialButton1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Consolas", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4, 3, 4, 3)
+        Me.MaximizeBox = False
+        Me.Name = "EasyImageConverter"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Convert you image here!"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SaveFileDialog1 As SaveFileDialog
+    Friend WithEvents MaterialButton1 As MaterialSkin.Controls.MaterialButton
+    Friend WithEvents MaterialButton2 As MaterialSkin.Controls.MaterialButton
+    Friend WithEvents MaterialButton3 As MaterialSkin.Controls.MaterialButton
+    Friend WithEvents MaterialTextBox1 As MaterialSkin.Controls.MaterialTextBox
+    Friend WithEvents MaterialTextBox2 As MaterialSkin.Controls.MaterialTextBox
+End Class
---
+++ EasyImageConverter.vb
@@ -0,0 +1,86 @@
+﻿Imports System.Threading
+Public Class EasyImageConverter
+    Dim formats() As String = {"jpg", "png", "bmp", "jpeg", "jfif", "jpe", "rle", "dib", "svg", "svgz"}
+    Private Sub EasyImageConverter_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckForIllegalCrossThreadCalls = False
+        Process.GetCurrentProcess().PriorityClass = ProcessPriorityClass.High
+        Dim totalString As String = "All files (*.*)|*.*"
+        For Each format As String In formats
+            totalString += "|" + format.ToUpper() + " Image (*." + format + ")|*." + format
+        Next
+        OpenFileDialog1.Filter = totalString
+        SaveFileDialog1.Filter = totalString
+        OpenFileDialog1.DefaultExt = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+        SaveFileDialog1.DefaultExt = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+    End Sub
+    Private Sub MaterialButton1_Click(sender As Object, e As EventArgs) Handles MaterialButton1.Click
+        Dim thread As Thread = New Thread(New ThreadStart(Sub() ConvertImage()))
+        thread.Start()
+    End Sub
+    Private Sub MaterialButton2_Click(sender As Object, e As EventArgs) Handles MaterialButton2.Click
+        If OpenFileDialog1.ShowDialog() = DialogResult.OK Then
+            MaterialTextBox1.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+    Private Sub MaterialButton3_Click(sender As Object, e As EventArgs) Handles MaterialButton3.Click
+        If SaveFileDialog1.ShowDialog() = DialogResult.OK Then
+            MaterialTextBox2.Text = SaveFileDialog1.FileName
+        End If
+    End Sub
+    Public Sub EnableAll(ByVal toggle As Boolean)
+        MaterialButton1.Enabled = toggle
+        MaterialButton2.Enabled = toggle
+        MaterialButton3.Enabled = toggle
+        MaterialTextBox1.Enabled = toggle
+        MaterialTextBox2.Enabled = toggle
+    End Sub
+    Public Sub ConvertImage()
+        EnableAll(False)
+        If System.IO.File.Exists(MaterialTextBox1.Text) Then
+            Dim extension As String = System.IO.Path.GetExtension(MaterialTextBox1.Text).ToLower().Replace(".", "")
+            Dim continueThat As Boolean = False
+            For Each format As String In formats
+                If format = extension Then
+                    continueThat = True
+                    Exit For
+                End If
+            Next
+            If continueThat Then
+                Try
+                    extension = System.IO.Path.GetExtension(MaterialTextBox2.Text).ToLower().Replace(".", "")
+                    continueThat = False
+                    For Each format As String In formats
+                        If format = extension Then
+                            continueThat = True
+                        End If
+                    Next
+                    If continueThat Then
+                        If System.IO.File.Exists(MaterialTextBox2.Text) Then
+                            Try
+                                System.IO.File.Delete(MaterialTextBox2.Text)
+                            Catch ex As Exception
+                                MessageBox.Show("Tried to delete the existing output image file specified but it cannot be deleted!", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
+                                EnableAll(True)
+                                Exit Sub
+                            End Try
+                        End If
+                        Dim image As Bitmap = New Bitmap(MaterialTextBox1.Text)
+                        image.Save(MaterialTextBox2.Text)
+                        MessageBox.Show("Succesfully converted this image file!", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Information)
+                    Else
+                        MessageBox.Show("Invalid output image file format.", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+                Catch ex As Exception
+                    MessageBox.Show("Can not convert this image file!", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End Try
+            Else
+                MessageBox.Show("Invalid input image file format.", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+        Else
+            MessageBox.Show("Can not find this image file.", Me.Text, MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+        OpenFileDialog1.FileName = ""
+        SaveFileDialog1.FileName = ""
+        EnableAll(True)
+    End Sub
+End Class
